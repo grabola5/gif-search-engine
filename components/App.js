0d3abc457dcda0b9386f2c1cf08fd8a30afaf916
Aleksandra Grabska <gucwa.ola@gmail.com>
2019-09-05 11:28:49 +0200

    Use a promise

--- components/App.js
+++ components/App.js
@@ -15,31 +15,40 @@ App = React.createClass({
 		this.setState({
 			loading: true
 		});
-		this.getGif(searchingText, function(gif) {
+		
+		this.getGif(searchingText) 
+		.then(gif => {
 			this.setState({
 				loading: false,
 				gif: gif,
 				searchingText: searchingText
-			});
-		}.bind(this));
+				})
+			})
+		.catch(error => console.error('Something went wrong...', error))
 	},
-
-	getGif: function(searchingText, callback) {  
-    var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;  
-    var xhr = new XMLHttpRequest();  
-    xhr.open('GET', url);
-    xhr.onload = function() {
-        if (xhr.status === 200) {
-           var data = JSON.parse(xhr.responseText).data;
-            var gif = {  
-                url: data.fixed_width_downsampled_url,
-                sourceUrl: data.url
-            };
-            callback(gif);  
-        }
-    };
-    xhr.send();
-},
+	
+	getGif: function (searchingText) {
+		return new Promise(
+			function(resolve, reject) {
+				var url = GIPHY_API_URL + '/v1/gifs/random?api_key=' + GIPHY_PUB_KEY + '&tag=' + searchingText;  
+		    	var xhr = new XMLHttpRequest();
+		    	xhr.open('GET', url);
+		    	xhr.onload = function() {
+			   		if (xhr.status === 200) {
+		           		var data= JSON.parse(xhr.responseText).data;
+		            	var gif = {  
+		                	url: data.fixed_width_downsampled_url,
+		               		sourceUrl: data.url
+	            		};
+						resolve(gif);
+					} else {
+						reject (newError(this.statusText));
+					}
+	   			};
+	   			xhr.send();
+	    	}
+	    );
+	},
 
 	render: function() {
 		var styles = {
